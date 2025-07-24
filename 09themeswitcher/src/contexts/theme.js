33ba89130374eeae50036f13df6f83a415bd310f
ayash2004 <yasha9340@gmail.com>
2025-07-24 16:36:30 +0530

    Context API

---
+++ 09themeswitcher/src/contexts/theme.js
@@ -0,0 +1,17 @@
+import {createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: 'light',
+    darktheme: () => {
+        
+    },
+    lighttheme: () => {
+
+    }
+})
+
+export const ThemeProvider= ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
